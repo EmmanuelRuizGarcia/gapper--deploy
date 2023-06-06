e388165f22e361276b0288b9162900934b18245d
Emmanuel <emmanuel.ruiz.garcia@outlook.com>
2023-06-06 11:00:29 -0500

    Solution merge

--- src/components/SwiperSlides/SwiperSlides.jsx
+++ src/components/SwiperSlides/SwiperSlides.jsx
@@ -23,6 +23,29 @@ const SwiperSlides = ({ clients }) => {
           speed={800}
           autoplay={{ delay: 3000 }}
           modules={[Autoplay]}
+          breakpoints={{
+            0: {
+              slidesPerView: 1,
+            },
+            400: {
+              slidesPerView: 2,
+            },
+            639: {
+              slidesPerView: 3,
+            },
+            865: {
+              slidesPerView: 3,
+            },
+            1000: {
+              slidesPerView: 3,
+            },
+            1500: {
+              slidesPerView: 3,
+            },
+            1700: {
+              slidesPerView: 3,
+            },
+          }}
         >
           {clients.map((client, i) => (
             <SwiperSlide key={i}>
